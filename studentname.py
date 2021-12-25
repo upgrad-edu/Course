09b2c06065be417f9155309449c2d4f13ec4d8b1
R <devikar@hpe.com>
2021-12-25 08:47:01 +0530

    Course1 edited

--- studentname.py
+++
@@ -1,7 +0,0 @@
-name = input("What is you name")
-if name[0] in 'ABCDEFGH':
-   print("1. Your name \n2. The module name of your choice from Course 1 \n3. 4 sentences about your learnings from that module")
-elif name[0] in 'IJKLMNOP':
-    print("1. Your name \n2. The module name of your choice from Course 2 \n3. 4 sentences about your learnings from that module")
-elif name[0] in 'QRSTUVWXYZ':
-    print("1. Your name \n2. The module name of your choice from Course 3 \n3. 4 sentences about your learnings from that module")
