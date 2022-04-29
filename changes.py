313ee14e0ef556458ac526f73855e03cd73a0ae0
Sneha Kumari <Sneha.Kumari3@gds.ey.com>
2022-04-29 20:58:55 +0530

    changes made according to first letter of name

---
+++ changes.py
@@ -0,0 +1,15 @@
+
+def change1(string):
+    for char in string:
+        if char[0].isin('A','B','C','D','E','F','G','H'):
+            print('make changes to file Course1')
+
+def change2(string):
+    for char in string:
+        if char[0].isin('I','J','K','L','M','N','O','P'):
+            print('make changes to file Course2')
+
+def change3(string):
+    for char in string:
+        if char[0].isin('Q','R','S','T','U','V','W','X','Y','Z'):
+            print('make changes to file Course3')                        
