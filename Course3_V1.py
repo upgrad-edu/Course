5a0829f7a0bebdb8b20c811784394179e1e33192
Sa2019Si <sagarika.bhadra@gmail.com>
2023-01-29 03:08:03 +0530

    Adding comments for Course3

---
+++ Course3_V1.py
@@ -0,0 +1,14 @@
+What are the key concepts you learnt from these courses
+You can choose one module from the list below:-
+1)Algorithm Analysis
+2)Divide and Conquer Algorithms
+3)Dynamic Programming
+
+
+Pawan Dubey
+Dynamic Programming
+Understanding the prolem and implementing a solution which is space and time efficient by structuring it into sub problems.
+
+Sagarika Bhadra
+The term dynamic programming refers to a collection of algorithms which can be used to compute optimal policies given a perfect model of the environment as a Markov decision process
+
