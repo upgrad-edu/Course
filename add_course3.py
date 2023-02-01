0d6d588ec78b1edd126bc4ba0a4576c74445da6e
vkarkera <vafrunkarkera17@gmail.com>
2023-02-01 11:09:56 +0530

    Course 3 coz of my name and fav module EDA

---
+++ add_course3.py
@@ -0,0 +1,4 @@
+# I am gonna change Course 3 coz my name starts with V
+
+Print("My name starts with V so Course 3 Changes...")
+Print("I like EDA, because of hoe insights can be drawn from a huge chunk of data using analysis and models.")
