d1cd50ac2838953d45b86bb30a8fbcf85393e2db
GangaGowthami <2gowthami@gmail.com>
2023-04-27 15:57:22 +0530

    addind new train function and Course1 changes

---
+++ train.py
@@ -0,0 +1,16 @@
+
+
+Name:Gowthami
+    What are the key concepts you learnt from these courses
+   
+  A)1.In this session i learnt how to install git and github.how to create branches and files in that branch.
+    2.How to track,commit and push the file into repository and also push-merging the branches.
+    3.Resetting and Reverting.
+    4.Some basic git commands.
+
+    You can choose one module from the list below:-
+    1)Algorithm Analysis
+    2)Divide and Conquer Algorithms
+    3)Dynamic Programming
+
+   A) Dynamic Programming
